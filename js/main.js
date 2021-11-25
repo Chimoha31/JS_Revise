3ef672b8b6f43b5936629ffad82f1acb59acc693
Chimoha31 <mokochii1108@gmail.com>
2021-11-24 18:54:26 -0800

    Fixed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-'use strict';
-
+"use strict";
 
 // const increment = function() {
 //   for(let i = 1; i <= 3; i++) {
@@ -9,11 +8,11 @@
 
 // increment();
 
-const increment1 = function() {
+const increment1 = function () {
   let num = 0;
   num++;
   console.log(num);
-}
+};
 
 increment1();
 increment1();
@@ -24,41 +23,37 @@ increment1();
  * 以下のコードではエラーが発生します。
  * コンソールで"fn called"と表示されるように
  * fn内のコードを変更してください。
- * 
+ *
  * ※if文は削除してはいけません。
  */
-let a;
 
 function fn() {
-    if(true) {
-        a = 'fn called';
-    }
-    return a; // ReferenceError: a is not defined
+  let a;
+  if (true) {
+    a = "fn called";
+  }
+  return a; // ReferenceError: a is not defined
 }
 
 const result = fn();
 console.log(result);
-
-
 
 /**
  * 問題２：
  * fn2内の記述を変更して、各コンソールで
  * 期待値を出力するように修正してください。
  */
-var val;
+var val = 'val1';
 
 function fn2() {
-    val = 'val1';
-    console.log(val); // 期待値->'val1'
+  console.log(val); // 期待値->'val1'
+  
+  if (true) {
+    let val = "val2";
+    console.log(val); // 期待値->'val2'
+  }
 
-    var val;
-    if(true) {
-        val = 'val2';
-        console.log(val); // 期待値->'val2'
-    }
-
-    console.log(val); // 期待値->'val1'
+  console.log(val); // 期待値->'val1'
 }
 fn2();
 
@@ -67,20 +62,21 @@ fn2();
  * 「クロージャー（プライベート変数）」のレクチャーで作成
  * したincrementと同じ機能を持つincrement関数をブロック
  * スコープとクロージャーを利用して作成してみてください。
- * 
+ *
  * increment(); // 期待値->1
  * increment(); // 期待値->2
  * increment(); // 期待値->3
  * increment(); // 期待値->4
  */
-
- let num = 0;
-function increment() {
+{
+  let num = 0;
+  function increment() {
     num = num + 1;
     console.log(num);
     // return increment;
+  }
 }
-
+  
 increment();
 increment();
 increment();
