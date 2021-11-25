36c0ac00959eaa00a1164ad026d0ebfe82e79315
Chimoha31 <mokochii1108@gmail.com>
2021-11-25 02:30:44 -0800

    type

--- js/main.js
+++ js/main.js
@@ -133,3 +133,22 @@ calc.plus(5);
 calc.minus(3);    
 calc.multiply(3);  
 calc.divide(2);    
+
+function printTypeAndValue(val) {
+  console.log(typeof val, val);
+}
+
+let a = 0;
+printTypeAndValue(a);
+
+let b = '1' + a;
+printTypeAndValue(b);
+
+let c = 15 - b;
+printTypeAndValue(c);
+
+let d = c - null;
+printTypeAndValue(d);
+
+let e = d - true;
+printTypeAndValue(e);
