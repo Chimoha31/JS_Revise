f764c7a48cf5bacf0066db1c076e3a47cd98bbb1
Chimoha31 <mokochii1108@gmail.com>
2021-11-29 21:57:45 -0800

    answer

--- js/main03.js
+++ js/main03.js
@@ -131,3 +131,39 @@ calc3.plus(5);
 calc3.minus(3); 
 calc3.multiply(3);
 calc3.divide(2);
+
+
+/**
+ * 以下のコードはクロージャーのレクチャーで作成したコードです。
+ * これをアロー関数を用いてなるべく短くなるように記載してみてください。
+ * 
+ * 問題１：
+ * addNumberFactoryを短く書き直してみましょう。
+ */
+const addNumberFactory = num => value => num + value;
+
+const add5 = addNumberFactory(5);
+const result = add5(10);
+console.log(result);
+
+/**
+ * 問題２：
+ * incrementFactoryを短く書き直してみましょう。
+ */
+const incrementFactory = () => {
+    
+    let num = 0;
+
+    return function a() {
+        num++;
+        console.log(num);
+    }
+
+}
+
+const increment = incrementFactory();
+
+increment();
+increment();
+increment();
+increment();
