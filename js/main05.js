3007d32d3f1a92de85904de2d2336b8cbe9fec03
Chimoha31 <mokochii1108@gmail.com>
2021-12-09 01:18:50 -0800

    Promise

--- js/main05.js
+++ js/main05.js
@@ -1,28 +1,53 @@
 "use strict";
 
-const btn = document.querySelector("button");
-btn.addEventListener("click", function task() {
-  console.log("task2 done");
-});
+// const btn = document.querySelector("button");
+// btn.addEventListener("click", function task() {
+//   console.log("task2 done");
+// });
 
-function a() {
-  setTimeout(function task() {
-    console.log("task1 done");
-  }, 4000);
-}
-const startTime = new Date();
-while (new Date() - startTime < 5000);
-console.log("fn a done");
+// function a() {
+//   setTimeout(function task() {
+//     console.log("task1 done");
+//   }, 4000);
+// }
+// const startTime = new Date();
+// while (new Date() - startTime < 5000);
+// console.log("fn a done");
 
-a();
+// a();
 
-new Promise(function (resolve, reject) {
-  reject("bye");
-})
-  .then()
-  .catch(function(data) {
-    console.log('data') //bye
-  })
-  .finally(function () {
-    console.log("終了処理");
+// new Promise(function (resolve, reject) {
+//   reject("bye");
+// })
+//   .then()
+//   .catch(function(data) {
+//     console.log('data') //bye
+//   })
+//   .finally(function () {
+//     console.log("終了処理");
+//   });
+
+function sleep(val) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log(val++);
+      resolve(val);
+    }, val * 500);
   });
+}
+
+Promise.all([sleep(2), sleep(3), sleep(4)]).then(function (data) {
+  console.log(data);
+});
+// Promise.race([sleep(2), sleep(3), sleep(4)]).then(function (data) {
+//   console.log(data);
+// });
+// sleep(0).then(function(val) {
+//   return Promise.all([sleep(2), sleep(3), sleep(4)]);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// })
