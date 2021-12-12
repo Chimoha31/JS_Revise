a7d985285e59567cdcc5149d4fc3994fb180010c
Chimoha31 <mokochii1108@gmail.com>
2021-12-11 22:08:38 -0800

    created module.js

--- js/main06.js
+++ js/main06.js
@@ -1,93 +1,109 @@
 // 'use strict';
 
-const targetObj = { a: 0 };
-const handler = {
-  set: function(target, prop, value, receiver) {
-    console.log(`[set]: ${prop}`);
-    target[prop] = value;
-    // throw new Error('cannot add prop');
-  },
-  get: function(target, prop, receiver) {
-    console.log(receiver);
-    // 値が見つからなかった時のデフォルト値
-    if(target.hasOwnProperty(prop)){
-      return target[prop];
-    }else{
-      return '-1';
-    }
-    console.log(`[get]: ${prop}`);
-    return target[prop];
-  },
-  deleteProperty: function(target, prop) {
-    console.log(`[delete]: ${prop}`);
-    delete target[prop];
-    // throw new Error('cannot delete prop');
-  }
-}
-const pxy = new Proxy(targetObj, handler);
-// pxy.a = 1;
-console.log(pxy.b);
-// pxy.b;
-delete pxy.a;
+// console.log(tim._name);
 
-// -----------------------------
-class C {
-  constructor(a, b) {
-    this.a = a;
-    this.b = b;
-  }
-}
+// const targetObj = { a: 0 };
+// const handler = {
+//   set: function(target, prop, value, receiver) {
+//     console.log(`[set]: ${prop}`);
+//     target[prop] = value;
+//     // throw new Error('cannot add prop');
+//   },
+//   get: function(target, prop, receiver) {
+//     console.log(receiver);
+//     // 値が見つからなかった時のデフォルト値
+//     if(target.hasOwnProperty(prop)){
+//       return target[prop];
+//     }else{
+//       return '-1';
+//     }
+//     console.log(`[get]: ${prop}`);
+//     return target[prop];
+//   },
+//   deleteProperty: function(target, prop) {
+//     console.log(`[delete]: ${prop}`);
+//     delete target[prop];
+//     // throw new Error('cannot delete prop');
+//   }
+// }
+// const pxy = new Proxy(targetObj, handler);
+// // pxy.a = 1;
+// console.log(pxy.b);
+// // pxy.b;
+// delete pxy.a;
 
-const obj1 = new C(1, 2);
-console.log(obj1); // = Reflect.constructを呼んでいるのと同じ
-const obj2 = Reflect.construct(C, [1,2]);
-console.log(obj2);
+// // -----------------------------
+// class C {
+//   constructor(a, b) {
+//     this.a = a;
+//     this.b = b;
+//   }
+// }
 
-console.log('a' in obj1); //true
-console.log('c' in obj1); //false
-console.log(Reflect.has(obj1, 'b'));
-//------------------------------
+// const obj1 = new C(1, 2);
+// console.log(obj1); // = Reflect.constructを呼んでいるのと同じ
+// const obj2 = Reflect.construct(C, [1,2]);
+// console.log(obj2);
 
-const bob = {
-  name: 'Bob',
-  _hello: function() {
-    console.log(`hello ${this.name}`);
-  }
-}
+// console.log('a' in obj1); //true
+// console.log('c' in obj1); //false
+// console.log(Reflect.has(obj1, 'b'));
+// //------------------------------
 
-const tom = {
-  name: 'Tom',
-  _hello: function() {
-    console.log(`hello ${this.name}`);
-  },
-  get hello() {
-    console.log(this);
-    return this._hello();
-  }
-}
+// const bob = {
+//   name: 'Bob',
+//   _hello: function() {
+//     console.log(`hello ${this.name}`);
+//   }
+// }
 
-tom.hello;
-Reflect.get(tom, 'hello', bob);
-//--------------------------------
+// const tom = {
+//   name: 'Tom',
+//   _hello: function() {
+//     console.log(`hello ${this.name}`);
+//   },
+//   get hello() {
+//     console.log(this);
+//     return this._hello();
+//   }
+// }
 
-const targetObj02 = { 
-  a: 0,
-  get value() {
-    return this.b;
-  }
-};
+// tom.hello;
+// Reflect.get(tom, 'hello', bob);
+// //--------------------------------
 
-const handler02 = {
-  get: function(target, prop, receiver) {
-    console.log(`[get]: ${prop}`);
-    if(target.hasOwnProperty(prop)) {
-      // return target[prop];
-      return Reflect.get(target, prop, receiver);
-    } else {
-      return -1;
-    }
-  }
-}
-const pxy02 = new Proxy(targetObj02, handler02);
-console.log(pxy02.value);
-console.log(pxy02.b);
+// const targetObj02 = { 
+//   a: 0,
+//   get value() {
+//     return this.b;
+//   }
+// };
+
+// const handler02 = {
+//   get: function(target, prop, receiver) {
+//     console.log(`[get]: ${prop}`);
+//     if(target.hasOwnProperty(prop)) {
+//       // return target[prop];
+//       return Reflect.get(target, prop, receiver);
+//     } else {
+//       return -1;
+//     }
+//   }
+// }
+// const pxy02 = new Proxy(targetObj02, handler02);
+// console.log(pxy02.value);
+// console.log(pxy02.b);
+// //--------------------------------------------
+
+// const wm = new WeakMap();
+
+// let o = {};
+// //値を設定する'set()'
+// wm.set(o, 'value1');
+
+
+// console.log(wm.get(o));
+// console.log(wm.delete(o));
+// console.log(wm.get(o));
+//--------------------------------------------
+
