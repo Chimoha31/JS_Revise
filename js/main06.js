8e566a7653225994ed155c7f19ad124ef8994dab
Chimoha31 <mokochii1108@gmail.com>
2021-12-11 22:49:43 -0800

    JSON

--- js/main06.js
+++ js/main06.js
@@ -1,10 +1,10 @@
 // 'use strict';
-import {Person} from './person.js';
+// import {Person} from './person.js';
 
-const tim = new Person('Tim');
-const bob = new Person('Bob');
-tim.hello();
-bob.hello();
+// const tim = new Person('Tim');
+// const bob = new Person('Bob');
+// tim.hello();
+// bob.hello();
 // console.log(tim._name);
 
 // const targetObj = { a: 0 };
@@ -111,4 +111,16 @@ bob.hello();
 // console.log(wm.delete(o));
 // console.log(wm.get(o));
 //--------------------------------------------
+const obj = {a: 0, b: 1, c: 2};
 
+function replacer(prop, value) {
+  if(value < 1) {
+    return;
+  }
+  return value;
+}
+const json = JSON.stringify(obj, ["a", "b"]);
+console.log(json);
+
+const obj2 = JSON.parse(json);
+console.log(obj2);
