1be647d400fdb4bb25501bdc2898f95b6fb1b49f
Chimoha31 <mokochii1108@gmail.com>
2021-12-11 21:27:53 -0800

    Reflect and Proxy

--- js/main06.js
+++ js/main06.js
@@ -29,3 +29,65 @@ const pxy = new Proxy(targetObj, handler);
 console.log(pxy.b);
 // pxy.b;
 delete pxy.a;
+
+// -----------------------------
+class C {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+}
+
+const obj1 = new C(1, 2);
+console.log(obj1); // = Reflect.constructを呼んでいるのと同じ
+const obj2 = Reflect.construct(C, [1,2]);
+console.log(obj2);
+
+console.log('a' in obj1); //true
+console.log('c' in obj1); //false
+console.log(Reflect.has(obj1, 'b'));
+//------------------------------
+
+const bob = {
+  name: 'Bob',
+  _hello: function() {
+    console.log(`hello ${this.name}`);
+  }
+}
+
+const tom = {
+  name: 'Tom',
+  _hello: function() {
+    console.log(`hello ${this.name}`);
+  },
+  get hello() {
+    console.log(this);
+    return this._hello();
+  }
+}
+
+tom.hello;
+Reflect.get(tom, 'hello', bob);
+//--------------------------------
+
+const targetObj02 = { 
+  a: 0,
+  get value() {
+    return this.b;
+  }
+};
+
+const handler02 = {
+  get: function(target, prop, receiver) {
+    console.log(`[get]: ${prop}`);
+    if(target.hasOwnProperty(prop)) {
+      // return target[prop];
+      return Reflect.get(target, prop, receiver);
+    } else {
+      return -1;
+    }
+  }
+}
+const pxy02 = new Proxy(targetObj02, handler02);
+console.log(pxy02.value);
+console.log(pxy02.b);
