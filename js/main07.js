44a2eb9957e3d08c1694d07ddfb27d755caee870
Chimoha31 <mokochii1108@gmail.com>
2021-12-13 00:52:27 -0800

    array

--- js/main07.js
+++ js/main07.js
@@ -4,17 +4,45 @@ const json = JSON.stringify(obj);
 localStorage.setItem('key', json);
 // localStorage.setItem('key2', '1');
 const result = localStorage.getItem('key');
+console.log(result);
 const obj2 = JSON.parse(result);
 console.log(obj2);
 // ------------------------------------
 
 const arry = [1, 2, 3, 4, 5];
 // const arry2 = arry.concat([6, 7, 8]);
-const arry2 = ([0, ...arry, 6, 7, 8])
+const arry02 = ([0, ...arry, 6, 7, 8])
 
 arry.push(6);
 arry.pop();
 // arry.shift();
 arry.unshift(0);
 const result02 = arry.splice(0, 2, 111, 222);
-console.log(arry2, result02);
+console.log(arry02, result02);
+// ------------------------------------
+const arry03 = [1, 2, 3, 4, 5];
+
+arry03.forEach(function(v, i, arry){
+  console.log(v);
+});
+
+const newArry = arry03.map(function(v, i, arry) {
+  return v * 2;
+});
+console.log(arry03, newArry);
+
+const filterArry = arry03.filter(function(v, i, arry) {
+  return i >= 1;
+});
+console.log(filterArry);
+// ------------------------------------
+
+const arry04 = [1, 2, 3, 4, 5];
+
+const result03 = arry04.reduce(function(accu, curr) {
+  console.log(accu, curr);
+  return accu + curr * 2;
+}, 0);
+
+console.log(result03);
+
